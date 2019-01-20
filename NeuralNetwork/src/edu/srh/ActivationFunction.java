e99dc2ee3500d210cfe14faafaf50f4cebfe75ad
Vivek <Vivek@172.19.165.208>
2019-01-20 23:13:29 +0100

    Neural Network

---
+++ NeuralNetwork/src/edu/srh/ActivationFunction.java
@@ -0,0 +1,15 @@
+/**
+ * Neural networks activation function interface.
+ */
+public interface ActivationFunction {
+	/**
+	 * Performs calculation based on the sum of input neurons output.
+	 * 
+	 * @param summedInput
+	 *            neuron's sum of outputs respectively inputs for the connected
+	 *            neuron
+	 * 
+	 * @return Output's calculation based on the sum of inputs
+	 */
+	double calculateOutput(double summedInput);
+}
